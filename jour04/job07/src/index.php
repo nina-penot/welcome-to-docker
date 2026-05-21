47f6aae5100d4496258a82d7969035a39905f4c9
nina-penot <nina.penot@laplateforme.io>
2026-05-21 16:26:53 +0200

    Activation de php et phpmyadmin dans dockercompose

--- jour04/job07/src/index.php
+++ jour04/job07/src/index.php
@@ -1,16 +1,37 @@
 <?php
 phpinfo();
 
-$DB_NAME = getenv("DB_NAME") or "db";
-$DB_USER = getenv("DB_USER") or "myuser";
-$DB_PASS = getenv("DB_PASS") or "pass";
-$DB_HOST = getenv("DB_HOST") or "db";
+$DB_NAME = getenv("MYSQL_DATABASE") ? getenv("MYSQL_DATABASE") : "db";
+$DB_USER = getenv("MYSQL_USER") ? getenv("MYSQL_USER") : "dev";
+$DB_PASS = getenv("MYSQL_PASSWORD") ? getenv("MYSQL_PASSWORD") : "pass";
+$DB_HOST = getenv("DB_HOST") ? getenv("DB_HOST") : "db";
+
+try {
+    $db_pdo = new PDO("mysql:host=db;dbname=lamp_demo", "dev", "pass");
+    if ($db_pdo) {
+        echo ("Connecté à la base.");
+    }
+} catch (PDOException $err) {
+    echo ($err);
+}
 
 // try {
-//     $db_pdo = new PDO("mysql:host=" . $DB_HOST . "dbname=" . $DB_NAME, $DB_USER, $DB_PASS);
-//     if ($db_pdo) {
+//     $db = new SQLite3("db.db");
+//     if ($db) {
 //         echo ("Connecté à la base.");
+//         print_r($db);
 //     }
-// } catch (PDOException $err) {
+// } catch (Error $err) {
 //     echo ($err);
+// }
+
+// try {
+//     $db = mysqli_connect("db", "dev", "pass", "lamp_demo");
+//     if (!$db) {
+//         echo ("Could not find database");
+//     } else {
+//         echo ("Connexion to database successful");
+//     }
+// } catch (Error $err) {
+//     print_r($err);
 // }
